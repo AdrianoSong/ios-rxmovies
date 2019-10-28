74a653375e6c9e30d28ffc93ecc30ea6e40adf02
Song <adriano.song@rga.com>
2019-10-28 20:06:05 -0300

    Initial Commit

---
+++ iOSRxMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSRxMovies
+//
+//  Created by Adriano Song on 28/10/19.
+//  Copyright © 2019 Adriano Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
